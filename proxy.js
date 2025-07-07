7653d896d1b5127d04e775a91c10f63526d132d4
OBS-Akuma <JuicerAkuma@gmail.com>
2025-07-07 13:22:07 -0400

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,53 +1,15 @@
-const express = require('express');
 const fetch = require('node-fetch');
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+(async () => {
+  const res = await fetch('https://kirka.io/');
+  let html = await res.text();
+  console.log('HTML length:', html.length);
 
-app.use(async (req, res) => {
-  try {
-    const targetUrl = `https://kirka.io${req.originalUrl}`;
-    const kirkaRes = await fetch(targetUrl);
+  html = html.replace(/<div[^>]+id=["']?crazygames-wrapper["']?[^>]*>[\s\S]*?<\/div>/gi, '');
+  html = html.replace(
+    /<script[^>]+src=["'][^"']*(crazygames|crazygames-sdk|cdn.embedly)[^"']*["'][^>]*><\/script>/gi,
+    ''
+  );
 
-    const contentType = kirkaRes.headers.get('content-type') || '';
-
-    if (contentType.includes('text/html')) {
-      let html = await kirkaRes.text();
-
-      // Remove CrazyGames wrapper div
-      html = html.replace(/<div[^>]+id=["']?crazygames-wrapper["']?[^>]*>[\s\S]*?<\/div>/gi, '');
-
-      // Remove external scripts that obviously belong to CrazyGames
-      html = html.replace(
-        /<script[^>]+src=["'][^"']*(crazygames|crazygames-sdk|cdn.embedly)[^"']*["'][^>]*><\/script>/gi,
-        ''
-      );
-
-      res.set('Content-Type', 'text/html');
-      res.send(html);
-
-    } else {
-      // Stream non-HTML content with all headers from Kirka.io response
-      kirkaRes.headers.forEach((value, name) => {
-        res.setHeader(name, value);
-      });
-
-      kirkaRes.body.pipe(res);
-
-      kirkaRes.body.on('error', err => {
-        console.error('Error streaming response:', err);
-        if (!res.headersSent) {
-          res.status(500).send('Error streaming response');
-        } else {
-          res.end();
-        }
-      });
-    }
-  } catch (err) {
-    res.status(500).send('Error fetching Kirka.io: ' + err.message);
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Proxy server listening on port ${PORT}`);
-});
+  console.log('Modified HTML length:', html.length);
+})();
